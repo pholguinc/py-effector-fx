ca7a282364a5030ea1e93d4811e7aeea71c02787
effector2 <konan.2021@gmail.com>
2026-01-18 10:51:50 -0500

    Add files via upload

--- effector2.lua
+++ effector2.lua
@@ -1,84 +1,63 @@
 	script_name		   = "effector2"
 	script_description = "New Generation Effects Automation Subs. Creating Effects with Modifiable Parameters"
 	script_author	   = "vict8r"
-	script_version	   = "1.0 beta"
-	script_update	   = "october 9th 2025"
+	script_version	   = "1.0.1 beta"
+	script_update	   = "january 18th 2026"
 	
 	include("karaskel.lua")
 	local ke = require("kelibs/newkara_library")
-	local ROUND_NUM = 3
 	
-	ke.config.runfx = function(subs, meta, orgline, sett, fx__, linefx, linei)
-		retime = ke.recall.retime	--retime("word", 0, 0)
-		maxloop = ke.recall.maxloop	--maxloop(3)
-		if fx__.fx_printfx then
-			ke.config.savefx(fx__)
-		else
-			ke.infofx.xres, ke.infofx.yres = aegisub.video_size()
-			ke.infofx.ratio = ke.math.round((ke.infofx.xres or 1280) / 1280, ROUND_NUM)
-			local msa, msb = aegisub.ms_from_frame(1), aegisub.ms_from_frame(101)
-			ke.infofx.frame_dur = msb and ke.math.round((msb - msa) / 100, ROUND_NUM) or 41.708
+	ke.config.runfx = function(subtitles, meta, styles, index, linefx, sett, fx__)
+		local data = ke.infofx.data
+		local heads = ke.infofx.sethead()
+		local fxcount, time_iii = 0, tonumber(os.time())
+		for li, xline in ipairs(index) do
+			local orgline, linei = subtitles[xline], linefx[li]
+			karaskel.preproc_line(subtitles, meta, styles, orgline)
 			local l = ke.table.copy(orgline)
-			local sets = {
-				["char"] = ke.config.text2char(l.text, l.duration, l.styleref, linei.left, linei.top),
-				["syl"]  = ke.config.text2syl(l.text, l.duration, l.styleref, linei.left, linei.top),
-				["word"] = ke.config.text2word(l.text, l.duration, l.styleref, linei.left, linei.top),
-				["line"] = {linei},
-			}
-			local arrayfx = sets[fx__.fx_type]
-			ke.infofx.l = linei
-			for _, fx in ipairs(arrayfx) do
-				local char = sets.char[fx.ci]
-				local syl  = sets.syl[fx.si]
-				local word = sets.word[fx.wi]
-				local line = ke.table.copy(orgline)
-				char.n, syl.n, word.n, line.n = #sets.char, #sets.syl, #sets.word, #linefx
-				local setn = {line = line.n, word = word.n, syl = syl.n, char = char.n}
-				fx.n = setn[fx__.fx_type]
-				ke.infofx.fx = fx
-				ke.infofx.times = {
-					char = {start_time = char.start_time, end_time = char.end_time},
-					syl  = {start_time = syl.start_time, end_time = syl.end_time},
-					word = {start_time = word.start_time, end_time = word.end_time},
-					line = {start_time = line.start_time, end_time = line.end_time}
-				}
-				--variables:
-				local svar = ("return function(fx__, line, word, syl, char, ke) %s end"):format(fx__.fx_variable)
-				if pcall(loadstring(svar)) then
-					loadstring(svar)()(fx__, line, word, syl, char, ke)
+			local sets = heads.setlibs(linei)
+			local fxset = sets[fx__.fx_type]
+			heads.env.set({fxkara = fxset})
+			data.l = linei
+			if not fx__.fx_modify then
+				for _, fx in ipairs(fxset) do
+					if fx.text ~= (fx__.fx_noblank and "" or "ke2") then
+						local char, syl, word, line, keep = heads.setcswl(sets, fx, linei, orgline, index)
+						local j, maxj, svar, vars = 1, heads.setvarloop(char, syl, word, line, fx)
+						fx.maxj = maxj
+						data.fx = fx
+						while fxgroup and j <= maxj do
+							local var = heads.setvariable(char, syl, word, line, svar, vars, j)
+							---------------------------------------------------------------------------------
+							data.j, data.maxj = j, maxj
+							ke.config.valbox(meta, char, syl, word, line, l, fx, var, j, maxj)
+							fx.add_tags = data.mod_addtags and data.mod_addtags or fx.add_tags
+							fx.add_tags = fx__.fx_keept ~= "" and keep .. fx.add_tags or fx.add_tags
+							---------------------------------------------------------------------------------
+							l.start_time, l.end_time, l.duration = fx.time_ini, fx.time_fin, fx.time_dur
+							l.text = ("{%s%s%s}%s"):format(fx.align, fx.pos, fx.add_tags, fx.returnfx)
+							l.layer, l.comment = fx.layer, false
+							subtitles.insert(#subtitles + 1, l)
+							---------------------------------------------------------------------------------
+							--local tm = tonumber(os.time()) - time_iii
+							--tm = ("%s:%02d:%02d"):format(math.floor(tm / 3600), math.floor(tm / 60) % 60, tm % 60)
+							--aegisub.progress.set(100 * fxcount / (#index * fx.n * maxj))
+							--aegisub.progress.task(("Lines: [%d/%d]  time: [%s]  Lines Generated: %d"):format(li, #index, tm, fxcount + 1))
+							---------------------------------------------------------------------------------
+							j = j + 1
+							maxj = fx.maxj
+							fxcount = fxcount + 1
+						end
+					end
 				end
-				local var = ke.string.loadstr(fx__.fx_variable, {var = var, syl = syl})
-				--loop:
-				local loop = {1}
-				local sloop = ("return function(fx__, line, word, syl, char, ke) return {%s} end"):format(fx__.fx_loop)
-				if pcall(loadstring(sloop)) then
-					loop = loadstring(sloop)()(fx__, line, word, syl, char, ke)
-				end
-				ke.infofx.loop = loop
-				local j, maxj = 1, 1
-				for k, v in pairs(loop)do
-					maxj = maxj * v
-				end
-				fx.maxj = maxj
-				while j <= maxj do
-					----------------------------------------------------------------------------
-					ke.infofx.j = j
-					ke.infofx.maxj = maxj
-					ke.config.valbox(fx__, meta, line, l, word, syl, char, fx, var, ke, j, maxj)
-					fx.add_tags = ke.infofx.mod_addtags and ke.infofx.mod_addtags or fx.add_tags
-					----------------------------------------------------------------------------
-					l.start_time = fx.time_ini
-					l.end_time = fx.time_fin
-					l.duration = fx.time_dur
-					l.text = ("{%s%s%s}%s"):format(fx.align, fx.pos, fx.add_tags, fx.returnfx)
-					l.layer = fx.layer
-					subs.append(l)
-					----------------------------------------------------------------------------
-					maxj = fx.maxj
-					j = j + 1
-				end
+				orgline.comment = true
+			else
+				orgline.text = ke.config.modifyline(fx__, meta, orgline, ke)
 			end
+			subtitles[xline] = orgline
 		end
+		heads.env.clear()
+		ke.recall.reset()
 	end
 	
 	aegisub.register_macro(script_name .. " " .. script_version, script_description, ke.config.macro)
